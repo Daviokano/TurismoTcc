3617004d084e66c1d854090d5ace00645948b0ae
Bia0107 <anabsilva0107@gmail.com>
2026-07-25 12:11:17 -0300

    terminando home

--- e-commerce/HorizonTravel/HorizonTravel/obj/Debug/net8.0/HorizonTravel.AssemblyInfo.cs
+++ e-commerce/HorizonTravel/HorizonTravel/obj/Debug/net8.0/HorizonTravel.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HorizonTravel")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f47c5fd8ff8c4b7726bbdce8e1c693e3aed01e6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5ab5781bc1a854bedab5bb73da217a2ff4ee8cc")]
 [assembly: System.Reflection.AssemblyProductAttribute("HorizonTravel")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HorizonTravel")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
